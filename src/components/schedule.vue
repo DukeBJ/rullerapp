37fa7062841bedc46bfcb736fff78563b49d32f4
dukebj <bjworld@gmail.com>
2020-11-03 09:28:55 +0300

    update

--- src/components/schedule.vue
+++ src/components/schedule.vue
@@ -10,35 +10,53 @@
       </div>
       <div class="card-body">
         <div class="client-name">
-          <h4>Заказчик</h4>
-          <div>{{ customer.name }}</div>
-          <div class="square-button">
-            <span class="msg"></span>
+          <div class="block-info">
+            <h4>Заказчик</h4>
+            <div>{{ customer.name }}</div>
           </div>
+          <nav>
+            <div class="square-button">
+              <span class="msg"></span>
+            </div>
+          </nav>
         </div>
         <div class="client-adress">
-          <div>{{ customer.adress }}</div>
-          <div class="square-button">
-            <span class="msg"></span>
+          <div class="block-info">
+            <div>{{ customer.adress }}</div>
           </div>
+          <nav>
+            <div class="square-button">
+              <span class="msg"></span>
+            </div>
+          </nav>
         </div>
         <div class="before-price">
-          <h4>Предварительный расчёт</h4>
-          <div>{{ customer.firstPrice }}</div>
-          <div class="square-button">
-            <span class="msg"></span>
+          <div class="block-info">
+            <h4>Предварительный расчёт</h4>
+            <div>{{ customer.firstPrice }}</div>
           </div>
+          <nav>
+            <div class="square-button">
+              <span class="msg"></span>
+            </div>
+          </nav>
         </div>
         <div class="manager">
-          <h4>Менеджер</h4>
-          <div>{{ customer.manager }}</div>
-          <div class="square-button">
-            <span class="msg"></span>
+          <div class="block-info">
+            <h4>Менеджер</h4>
+            <div>{{ customer.manager }}</div>
           </div>
+          <nav>
+            <div class="square-button">
+              <span class="msg"></span>
+            </div>
+          </nav>
         </div>
         <div class="comment">
-          <h4>Комментарии</h4>
-          <div>{{ customer.comment }}</div>
+          <div class="block-info">
+            <h4>Комментарии</h4>
+            <div>{{ customer.comment }}</div>
+          </div>
         </div>
         <button class="app-btn btn__blue">Начать замер</button>
         <button class="app-btn btn__blue">Отправить в расчёт</button>
